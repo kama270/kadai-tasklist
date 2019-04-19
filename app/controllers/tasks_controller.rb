dbb0766ff6130c84be51e07e25a0e07a42c18c8a
hiro <mylog.9102bis.tester@gmail.com>
2019-04-19 14:06:19 +0000

    validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -52,6 +52,6 @@ class TasksController < ApplicationController
     end
     
     def task_params
-        params.require(:task).permit(:cotent, :title)
+        params.require(:task).permit(:content, :status)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20190419095637) do
 
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "cotent"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "title"
     t.string   "status"
   end
 
