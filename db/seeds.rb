123224e7b327e33e88fb3f793f6183cadc02b9a0
hiro <mylog.9102bis.tester@gmail.com>
2019-04-20 15:43:03 +0000

    bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 (1..100).each do |number|
-    Task.create(title: 'test title ' + number.to_s, content: 'test content ' + number.to_s)
+  Task.create(status: 'MCU' + number.to_s, content: 'END GAME ' + number.to_s)
 end
